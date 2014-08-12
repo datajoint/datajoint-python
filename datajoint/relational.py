3bac6d1bb2cb6bd74f0527e427eee2a77e4ba6ac
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2014-08-11 20:48:01 -0500

    implemented restriction by sql conditions

--- datajoint/relational.py
+++ datajoint/relational.py
@@ -25,7 +25,6 @@ class Relational:
         
     def pro(self, *arg, _sub=None, **kwarg):
         "relational projection abd aggregation"
-        print('**SELECT**', arg)
         return Projection(self, _sub=_sub, *arg, **kwarg)
         
     def fetch(self, *arg, _limit=None, _offset=0, **kwarg):
@@ -34,9 +33,9 @@ class Relational:
         """
         sql, heading = self.pro(*arg,**kwarg)._compile()
         #TODO: implement offset and limt
-        print('SELECT '+heading.asSQL+' FROM ' + sql)
-        cur = self.conn.query('SELECT '+heading.asSQL+' FROM ' + sql)
-        ret = np.array(list(cur), dtype=heading.asdtype)
+        sql = 'SELECT '+heading.asSQL+' FROM ' + sql + self._whereClause
+        print(sql)
+        ret = np.array(list(self.conn.query(sql)), dtype=heading.asdtype)
         # unpack blobs
         for i in range(len(ret)):
             for f in heading.blobs:
@@ -46,6 +45,7 @@ class Relational:
     def __and__(self, restriction):
         "relational restriction or semijoin"
         ret = copy(self)
+        ret._restrictions = list(ret._restrictions)  # copy restiction
         ret &= restriction
         return ret
         
@@ -62,12 +62,32 @@ class Relational:
     def __sub__(self, restriction):
         "inverted restriction or antijoin"
         return self & Not(restriction)
+        
+    @property
+    def _whereClause(self):
+        "apply restriction to sql statement"
+        if not self._restrictions:
+            sql = ''
+        else:
+            condStr = []
+            for r in self._restrictions:
+                negate = isinstance(r,Not)
+                if negate:
+                    r = r._restriction
+                #TODO: imlement restriction by dict and np.array
+                assert isinstance(r,str), 'condition must be converted into a string'
+                r = '('+r+')'
+                if negate:
+                    r = 'NOT '+r;
+                condStr.append(r)
+            sql = ' WHERE ' + ' AND '.join(condStr)
+        return sql
 
 
 class Not:
     "inverse of a restriction" 
     def __init__(self,restriction):
-        self.restriction = restriction
+        self._restriction = restriction
   
    
 class Join(Relational):
@@ -88,8 +108,9 @@ class Join(Relational):
         sql2, heading2 = self._rel2._compile()
         #TODO: incomplete
         heading = heading1.join(heading2)
-        sql = '%s NATURAL JOIN %s as `$t%x`' % (sql1, sql2) 
-        return sql, heading
+        sql = '%s NATURAL JOIN %s as `$t%x`' % (sql1, sql2, Join.aliasCounter)
+        Join.aliasCounter += 1
+        return sql+self._whereClause, heading
 
 
         
@@ -107,10 +128,9 @@ class Projection(Relational):
         self._sub = _sub        
         self._selection = arg
         self._renames = kwarg
-        print('Selection: ', self._selection)
         
     def _compile(self):
         sql, heading = self._rel._compile()
         heading = heading._pro(*self._selection, **self._renames)
-        # TODO: enclose in subquery if necessary
-        return sql, heading
+        # TODO: enclose subqueries
+        return sql + self._whereClause, heading
