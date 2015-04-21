12daafd9df4806e9cc2ba6dc9da00e2eaa64353a
Fabian Sinz <fabee@epagoge.de>
2015-04-21 09:47:02 -0500

    problems with database password

--- datajoint/connection.py
+++ datajoint/connection.py
@@ -38,6 +38,8 @@ def conn_container():
             host = host or os.getenv('DJ_HOST') or input('Enter datajoint server address >> ')
             user = user or os.getenv('DJ_USER') or input('Enter datajoint user name >> ')
             # had trouble with getpass
+            print(passwd)
+            print(os.getenv('DJ_PASS'))
             passwd = passwd or os.getenv('DJ_PASS') or input('Enter datajoint password >> ')
             initFun = initFun or os.getenv('DJ_INIT')
             _connObj = Connection(host, user, passwd, initFun)
