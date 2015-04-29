737566d87a737f4f5338daffba83545b8ec0d411
Fabian Sinz <fabee@epagoge.de>
2015-04-28 22:12:27 -0500

    started to write new settings object

--- datajoint/__init__.py
+++ datajoint/__init__.py
@@ -1,4 +1,7 @@
-__author__ = "Dimitri Yatsenko and Edgar Walker at Baylor College of Medicine"
+import logging
+import os
+
+__author__ = "Dimitri Yatsenko, Edgar Walker, and Fabian Sinz   at Baylor College of Medicine"
 __version__ = "0.2"
 
 class DataJointError(Exception):
@@ -8,14 +11,30 @@ class DataJointError(Exception):
     """
     pass
 
+
+from .settings import Config, logger
+
+# ----------- loads local configuration from file ----------------
+config = Config()
+local_config_file = os.path.expanduser(os.environ.get(config['settings']['local config var'], None))
+if local_config_file is None:
+    local_config_file = os.path.expanduser(config['settings']['local config file'])
+
+try:
+    logger.log(logging.INFO, "Loading local settings from {0:s}".format(local_config_file))
+    config.load(local_config_file)
+except:
+    logger.warn("Local config file {0:s} does not exist! Creating it.".format(local_config_file))
+    config.save(local_config_file)
+
+
+
 from .connection import conn, Connection
 from .base import Base
 from .task import TaskQueue
 from .autopopulate import AutoPopulate
 from . import blob
 from .relational import Not
-
-
 
 
 
