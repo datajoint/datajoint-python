77bad6d772a9a7589c03cee7b377c6a42c903e30
Fabian Sinz <fabee@epagoge.de>
2015-04-29 08:59:51 -0500

    settings object finished

--- datajoint/__init__.py
+++ datajoint/__init__.py
@@ -3,7 +3,11 @@ import os
 
 __author__ = "Dimitri Yatsenko, Edgar Walker, and Fabian Sinz   at Baylor College of Medicine"
 __version__ = "0.2"
+__all__ = ['__author__', '__version__',
+           'Connection', 'Heading', 'Base', 'Not',
+           'AutoPopulate', 'TaskQueue', 'conn', 'DataJointError', 'blob']
 
+# ------------ define datajoint error before the import hierarchy is flattened ------------
 class DataJointError(Exception):
     """
     Base class for errors specific to DataJoint internal
@@ -12,14 +16,16 @@ class DataJointError(Exception):
     pass
 
 
-from .settings import Config, logger
 
 # ----------- loads local configuration from file ----------------
+from .settings import Config, logger
 config = Config()
-local_config_file = os.path.expanduser(os.environ.get(config['settings']['local config var'], None))
+local_config_file = os.environ.get(config['settings']['local config var'], None)
 if local_config_file is None:
     local_config_file = os.path.expanduser(config['settings']['local config file'])
-
+else:
+    local_config_file = os.path.expanduser(local_config_file)
+    config['settings']['local config file'] = local_config_file
 try:
     logger.log(logging.INFO, "Loading local settings from {0:s}".format(local_config_file))
     config.load(local_config_file)
@@ -28,7 +34,7 @@ except:
     config.save(local_config_file)
 
 
-
+# ------------- flatten import hierarchy -------------------------
 from .connection import conn, Connection
 from .base import Base
 from .task import TaskQueue
@@ -38,6 +44,3 @@ from .relational import Not
 
 
 
-__all__ = ['__author__', '__version__',
-           'Connection', 'Heading', 'Base', 'Not',
-           'AutoPopulate', 'TaskQueue', 'conn', 'DataJointError', 'blob']
--- datajoint/settings.py
+++ datajoint/settings.py
@@ -1,18 +1,91 @@
 """
 Settings for DataJoint.
 """
+from . import DataJointError
+import json
+import pprint
+
 __author__ = 'eywalker'
 import logging
+import collections
 
-# Settings dictionary. Don't manipulate this directly
 
-class Config(object):
+validators = collections.defaultdict(lambda: lambda value: True)
+
+default = {
+    'database': {
+        'host': 'localhost',
+        'password': 'datajoint',
+        'user': 'datajoint'
+    },
+    'settings': {
+        'local config file': 'dj_local_conf.json',
+        'local config var': 'DJ_LOCAL_CONF'
+    }
+}
+
+class Config(collections.MutableMapping):
     """
-    Configuration object
+    Stores datajoint settings. Behaves like a dictionary, but applies validator functions
+    when certain keys are set.
+
+    The default parameters are stored in datajoint.settings.default . If a local config file
+    exists, the settings specified in this file override the default settings.
+
     """
 
+    def __init__(self, *args, **kwargs):
+        self._conf = dict(default)
+        self.update(dict(*args, **kwargs))  # use the free update to set keys
+
+    def __getitem__(self, key):
+        return self._conf[key]
+
+    def __setitem__(self, key, value):
+        if validators[key]:
+            self._conf[key] = value
+        else:
+            raise DataJointError(u'Validator for {0:s} did not pass'.format(key, ))
+
+    def __delitem__(self, key):
+        del self._conf[key]
+
+    def __iter__(self):
+        return iter(self._conf)
+
+    def __len__(self):
+        return len(self._conf)
+
+    def __str__(self):
+        return  pprint.pformat(self._conf, indent=4)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def save(self, filename=None):
+        """
+        Saves the settings in JSON format to the given file path.
+        :param filename: filename of the local JSON settings file. If None, the local config file is used.
+        """
+        if filename is None:
+            import datajoint as dj
+            filename = dj.config['settings']['local config file']
+        with open(filename, 'w') as fid:
+            json.dump(self._conf, fid)
+
+    def load(self, filename):
+        """
+        Updates the setting from config file in JSON format.
+
+        :param filename=None: filename of the local JSON settings file. If None, the local config file is used.
+        """
+        if filename is None:
+            import datajoint as dj
+            filename = dj.config['settings']['local config file']
+        with open(filename, 'r') as fid:
+            self.update(json.load(fid))
 
 
-
+#############################################################################
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG) #set package wide logger level TODO:make this respond to environmental variable
