211cf7e4ae7c1b784fd3268e5b702967056c66ea
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2015-05-14 10:47:30 -0500

    added more nose tests for blobs

--- tests/test_blob.py
+++ tests/test_blob.py
@@ -5,6 +5,14 @@ from numpy.testing import assert_array_equal
 
 
 def test_pack():
-    x = np.random.randn(10, 10)
+    x = np.random.randn(8, 10)
+    assert_array_equal(x, unpack(pack(x)), "Arrays do not match!")
 
+    x = np.random.randn(10)
+    assert_array_equal(x, unpack(pack(x)), "Arrays do not match!")
+
+    x = np.float32(np.random.randn(3, 4, 5))
+    assert_array_equal(x, unpack(pack(x)), "Arrays do not match!")
+
+    x = np.int16(np.random.randn(1, 2, 3))
     assert_array_equal(x, unpack(pack(x)), "Arrays do not match!")
