9b70f5706d2283edbbe2470b10c6c3dc09a85a4a
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2015-06-04 18:07:01 -0500

    made table_name a class property in the user relation classes

--- datajoint/user_relations.py
+++ datajoint/user_relations.py
@@ -5,23 +5,27 @@ from .utils import from_camel_case
 
 class ManualRelation(RelationClass):
     @property
-    def table_name(self):
-        return from_camel_case(self.__class__.__name__)
+    @classmethod
+    def table_name(cls):
+        return from_camel_case(cls.__name__)
 
 
 class LookupRelation(RelationClass):
     @property
-    def table_name(self):
-        return '#' + from_camel_case(self.__class__.__name__)
+    @classmethod
+    def table_name(cls):
+        return '#' + from_camel_case(cls.__name__)
 
 
 class ImportedRelation(RelationClass, AutoPopulate):
     @property
-    def table_name(self):
-        return "_" + from_camel_case(self.__class__.__name__)
+    @classmethod
+    def table_name(cls):
+        return "_" + from_camel_case(cls.__name__)
 
 
 class ComputedRelation(RelationClass, AutoPopulate):
     @property
-    def table_name(self):
-        return "__" + from_camel_case(self.__class__.__name__)
+    @classmethod
+    def table_name(cls):
+        return "__" + from_camel_case(cls.__name__)
