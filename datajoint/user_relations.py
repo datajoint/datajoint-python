574b8a185c32f164c2e935698ae77fc9e494ae45
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2015-08-21 20:10:32 -0500

    bugfix in declaring subordinate relations

--- datajoint/user_relations.py
+++ datajoint/user_relations.py
@@ -34,7 +34,6 @@ class MasterMeta(type):
         return super().__new__(cls, name, parents, dct)
 
 
-
 class Manual(Relation, metaclass=MasterMeta):
     """
     Inherit from this class if the table's values are entered manually.
@@ -48,7 +47,7 @@ class Manual(Relation, metaclass=MasterMeta):
         return from_camel_case(self.__class__.__name__)
 
 
-class Lookup(ClassedRelation):
+class Lookup(Relation):
     """
     Inherit from this class if the table's values are for lookup. This is
     currently equivalent to defining the table as Manual and serves semantic
@@ -70,7 +69,7 @@ class Lookup(ClassedRelation):
             self.insert(self.contents, ignore_errors=True)
 
 
-class Imported(ClassedRelation, AutoPopulate):
+class Imported(Relation, AutoPopulate, metaclass=MasterMeta):
     """
     Inherit from this class if the table's values are imported from external data sources.
     The inherited class must at least provide the function `_make_tuples`.
@@ -84,7 +83,7 @@ class Imported(ClassedRelation, AutoPopulate):
         return "_" + from_camel_case(self.__class__.__name__)
 
 
-class Computed(ClassedRelation, AutoPopulate):
+class Computed(Relation, AutoPopulate, metaclass=MasterMeta):
     """
     Inherit from this class if the table's values are computed from other relations in the schema.
     The inherited class must at least provide the function `_make_tuples`.
@@ -96,23 +95,3 @@ class Computed(ClassedRelation, AutoPopulate):
         :returns: the table name of the table formatted for mysql.
         """
         return "__" + from_camel_case(self.__class__.__name__)
-
-
-class Subordinate(dj.Relation):
-    """
-    Subordinate relation is declared within another relation class
-
-    :input master: instance of the master relation containing the suborinate relation
-    """
-
-    def __init__(self, master):
-        self._master = master
-
-    def table_name(self):
-        return self._master.table_name + '__' + from_camel_case(self.__class__.__name__)
-
-
-
-
-
-
