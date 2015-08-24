7a6600bd1e6fa76e6f906f97f5e2b914abf497e6
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2015-08-23 19:06:06 -0500

    removed the old implementation of Subordinate

--- datajoint/__init__.py
+++ datajoint/__init__.py
@@ -17,7 +17,7 @@ __all__ = ['__author__', '__version__',
            'config',
            'Connection', 'Heading', 'Relation', 'FreeRelation', 'Not',
            'Relation', 'schema',
-           'Manual', 'Lookup', 'Imported', 'Computed', 'Sub',
+           'Manual', 'Lookup', 'Imported', 'Computed', 'Sub'
            'conn', 'kill']
 
 
@@ -55,7 +55,7 @@ logger.setLevel(log_levels[config['loglevel']])
 # ------------- flatten import hierarchy -------------------------
 from .connection import conn, Connection
 from .relation import Relation
-from .user_relations import Manual, Lookup, Imported, Computed, Sub, Subordinate
+from .user_relations import Manual, Lookup, Imported, Computed, Sub
 from .relational_operand import Not
 from .heading import Heading
 from .schema import schema
--- datajoint/user_relations.py
+++ datajoint/user_relations.py
@@ -83,40 +83,4 @@ class Computed(Relation, AutoPopulate):
         :returns: the table name of the table formatted for mysql.
         """
         return "__" + from_camel_case(self.__class__.__name__)
-
-
-class Subordinate:
-    """
-    Mix-in to make computed tables subordinate.
-    This class is DEPRECATED and will be removed in a future version. Use dj.Sub instead.
-    """
-
-    @property
-    def populated_from(self):
-        """
-        Overrides the `populate_from` property because subtables should not be populated
-        directly.
-        :return: None
-        """
-        return None
-
-    def _make_tuples(self, key):
-        """
-        Overrides the `_make_tuples` property because subtables should not be populated
-        directly. Raises an error if this method is called (usually from populate of the
-        inheriting object).
-        :raises: NotImplementedError
-        """
-        raise NotImplementedError(
-            'This table is subordinate: it cannot be populated directly. Refer to its parent table.')
-
-    def progress(self):
-        """
-        Overrides the `progress` method because subtables should not be populated directly.
-        """
-        raise NotImplementedError(
-            'This table is subordinate: it cannot be populated directly. Refer to its parent table.')
-
-    def populate(self, *args, **kwargs):
-        raise NotImplementedError(
-            'This table is subordinate: it cannot be populated directly. Refer to its parent table.')
+    
