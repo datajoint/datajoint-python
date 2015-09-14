917f4c79e4dfe8d48a72e0f5ab2efc9cabc11092
Edgar Y. Walker <edgar.walker@gmail.com>
2015-09-14 01:46:30 -0500

    register database for monitoring

--- datajoint/schema.py
+++ datajoint/schema.py
@@ -11,11 +11,14 @@ logger = logging.getLogger(__name__)
 class schema:
     """
     A schema object can be used  as a decorator that associates a Relation class to a database as
-    well as a namespace for looking up foreign key references.
+    well as a namespace for looking up foreign key references in table declaration.
     """
 
     def __init__(self, database, context, connection=None):
         """
+        Associates the specified database with this schema object. If the target database does not exist
+        already, will attempt on creating the database.
+
         :param database: name of the database to associate the decorated class with
         :param context: dictionary for looking up foreign keys references, usually set to locals()
         :param connection: Connection object. Defaults to datajoint.conn()
@@ -27,8 +30,8 @@ class schema:
         self.context = context
 
         # if the database does not exist, create it
-        cur = connection.query("SHOW DATABASES LIKE '{database}'".format(database=database))
-        if cur.rowcount == 0:
+
+        if not self.exists:
             logger.info("Database `{database}` could not be found. "
                         "Attempting to create the database.".format(database=database))
             try:
@@ -39,9 +42,37 @@ class schema:
                                      " an attempt to create has failed. Check"
                                      " permissions.".format(database=database))
 
+        # TODO: replace with a call on connection object
+        connection.erm.register_database(database)
+
+    def drop(self):
+        """
+        Drop the associated database if it exists
+        """
+        if self.exists:
+            logger.info("Dropping `{database}`.".format(database=self.database))
+            try:
+                self.connection.query("DROP DATABASE `{database}`".format(database=self.database))
+                logger.info("Database `{database}` was dropped successfully.".format(database=self.database))
+            except pymysql.OperationalError:
+                raise DataJointError("An attempt to drop database named `{database}` "
+                                     "has failed. Check permissions.".format(database=self.database))
+        else:
+            logger.info("Database named `{database}` does not exist. Doing nothing.".format(database=self.database))
+
+    @property
+    def exists(self):
+        """
+        :return: true if the associated database exists on the server
+        """
+        cur = self.connection.query("SHOW DATABASES LIKE '{database}'".format(database=self.database))
+        return cur.rowcount > 0
+
+
+
     def __call__(self, cls):
         """
-        The decorator binds its argument class object to a database
+        Binds the passed in class object to a database. This is intended to be used as a decorator.
         :param cls: class to be decorated
         """
 
@@ -56,7 +87,7 @@ class schema:
             relation_class().declare()
 
         if issubclass(cls, Part):
-            raise DataJointError('The schema decorator should not apply to part relations')
+            raise DataJointError('The schema decorator should not be applied to Part relations')
 
         process_relation_class(cls, context=self.context)
 
@@ -72,9 +103,10 @@ class schema:
                 if is_sub:
                     parts.append(part)
                     part._master = cls
+                    # TODO: look into local namespace for the subclasses
                     process_relation_class(part, context=dict(self.context, **{cls.__name__: cls}))
                 elif issubclass(part, Relation):
-                    raise DataJointError('Part relations must subclass from datajoint.Part')
+                    raise DataJointError('Part relations must be a subclass of datajoint.Part')
 
         # invoke Relation._prepare() on class and its part relations.
         cls()._prepare()
