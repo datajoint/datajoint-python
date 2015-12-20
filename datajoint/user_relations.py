77bc77fe35905dddcc1b1ce8434de89322a28db6
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2015-12-20 00:51:06 -0600

    converted table_name to classproperty in user relations

--- datajoint/user_relations.py
+++ datajoint/user_relations.py
@@ -2,14 +2,22 @@
 Hosts the table tiers, user relations should be derived from.
 """
 
-import abc
 from .base_relation import BaseRelation
 from .autopopulate import AutoPopulate
 from .utils import from_camel_case
 from . import DataJointError
 
 
-class Part(BaseRelation, metaclass=abc.ABCMeta):
+class classproperty:
+
+    def __init__(self, f):
+        self.f = f
+
+    def __get__(self, obj, owner):
+        return self.f(owner)
+
+
+class Part(BaseRelation):
     """
     Inherit from this class if the table's values are details of an entry in another relation
     and if this table is populated by this relation. For example, the entries inheriting from
@@ -17,44 +25,44 @@ class Part(BaseRelation, metaclass=abc.ABCMeta):
     Part relations are implemented as classes inside classes.
     """
 
-    @property
-    def master(self):
-        if not hasattr(self, '_master'):
+    @classproperty
+    def master(cls):
+        if not hasattr(cls, '_master'):
             raise DataJointError(
                 'Part relations must be declared inside a base relation class')
-        return self._master
+        return cls._master
 
     @property
     def table_name(self):
         return self.master().table_name + '__' + from_camel_case(self.__class__.__name__)
 
 
-class Manual(BaseRelation, metaclass=abc.ABCMeta):
+class Manual(BaseRelation):
     """
     Inherit from this class if the table's values are entered manually.
     """
 
-    @property
-    def table_name(self):
+    @classproperty
+    def table_name(cls):
         """
         :returns: the table name of the table formatted for mysql.
         """
-        return from_camel_case(self.__class__.__name__)
+        return from_camel_case(cls.__name__)
 
 
-class Lookup(BaseRelation, metaclass=abc.ABCMeta):
+class Lookup(BaseRelation):
     """
     Inherit from this class if the table's values are for lookup. This is
     currently equivalent to defining the table as Manual and serves semantic
     purposes only.
     """
 
-    @property
-    def table_name(self):
+    @classproperty
+    def table_name(cls):
         """
         :returns: the table name of the table formatted for mysql.
         """
-        return '#' + from_camel_case(self.__class__.__name__)
+        return '#' + from_camel_case(cls.__name__)
 
     def _prepare(self):
         """
@@ -64,29 +72,29 @@ class Lookup(BaseRelation, metaclass=abc.ABCMeta):
             self.insert(self.contents, skip_duplicates=True)
 
 
-class Imported(BaseRelation, AutoPopulate, metaclass=abc.ABCMeta):
+class Imported(BaseRelation, AutoPopulate):
     """
     Inherit from this class if the table's values are imported from external data sources.
     The inherited class must at least provide the function `_make_tuples`.
     """
 
-    @property
-    def table_name(self):
+    @classproperty
+    def table_name(cls):
         """
         :returns: the table name of the table formatted for mysql.
         """
-        return "_" + from_camel_case(self.__class__.__name__)
+        return "_" + from_camel_case(cls.__name__)
 
 
-class Computed(BaseRelation, AutoPopulate, metaclass=abc.ABCMeta):
+class Computed(BaseRelation, AutoPopulate):
     """
     Inherit from this class if the table's values are computed from other relations in the schema.
     The inherited class must at least provide the function `_make_tuples`.
     """
 
-    @property
-    def table_name(self):
+    @classproperty
+    def table_name(cls):
         """
         :returns: the table name of the table formatted for mysql.
         """
-        return "__" + from_camel_case(self.__class__.__name__)
+        return "__" + from_camel_case(cls.__name__)
