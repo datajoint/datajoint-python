afe3c8f7782ffc8b051a84b1ca83f4c35848ec6d
Fabian Sinz <sinz@bcm.edu>
2016-01-12 12:27:35 -0600

    add test for null and skip insert bug

--- tests/test_nan.py
+++ tests/test_nan.py
@@ -15,12 +15,24 @@ class NanTest(dj.Manual):
     """
 
 
-def test_insert_nan():
-    rel = NanTest()
-    a = np.array([0, 1/3, np.nan, np.pi, np.nan])
-    rel.insert(((i, value) for i, value in enumerate(a)))
-    b = rel.fetch.order_by('id')['value']
-    assert_true((np.isnan(a) == np.isnan(b)).all(),
-                'incorrect handling of Nans')
-    assert_true(np.allclose(a[np.logical_not(np.isnan(a))], b[np.logical_not(np.isnan(b))]),
-                'incorrect storage of floats')
+class TestNaNInsert:
+    def __init__(self):
+        self.rel = NanTest()
+        with dj.config(safemode=False):
+            self.rel.delete()
+        a = np.array([0, 1/3, np.nan, np.pi, np.nan])
+        self.rel.insert(((i, value) for i, value in enumerate(a)))
+        self.a = a
+
+
+    def test_insert_nan(self):
+        """Test fetching of null values"""
+        b = self.rel.fetch.order_by('id')['value']
+        assert_true((np.isnan(self.a) == np.isnan(b)).all(),
+                    'incorrect handling of Nans')
+        assert_true(np.allclose(self.a[np.logical_not(np.isnan(self.a))], b[np.logical_not(np.isnan(b))]),
+                    'incorrect storage of floats')
+
+    def test_nulls_do_not_affect_primary_keys(self):
+        """Test against a case that previously caused a bug when skipping existing entries."""
+        self.rel.insert(((i, value) for i, value in enumerate(self.a)), skip_duplicates=True)
