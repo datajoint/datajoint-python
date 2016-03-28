7e03b2bf6ec70949deece1986a52453d3ebc182d
Edgar Y. Walker <edgar.walker@gmail.com>
2016-03-28 17:01:52 -0500

    Add advanced blob unpacking mechanism

--- datajoint/blob.py
+++ datajoint/blob.py
@@ -27,8 +27,176 @@ mxClassID = OrderedDict((
     ('mxUINT64_CLASS', np.dtype('uint64')),
     ('mxFUNCTION_CLASS', None)))
 
-reverseClassID = {dtype: i for i, dtype in enumerate(mxClassID.values())}
-dtypeList = list(mxClassID.values())
+rev_class_id = {dtype: i for i, dtype in enumerate(mxClassID.values())}
+dtype_list = list(mxClassID.values())
+
+decode_lookup = {
+    b'ZL123\0': zlib.decompress
+}
+
+class BlobReader(object):
+    def __init__(self, blob, simplify=True):
+        self._simplify = simplify
+        self._blob = blob
+        self._pos = 0
+
+    @property
+    def pos(self):
+        return self._pos
+
+    @pos.setter
+    def pos(self, val):
+        self._pos = val
+
+    def reset(self):
+        self.pos = 0
+
+    def decompress(self):
+        for pattern, decoder in decode_lookup.items():
+            if self._blob[self.pos:].startswith(pattern):
+                self.pos += len(pattern)
+                blob_size = self.read_value('uint64')
+                blob = decoder(self._blob[self.pos:])
+                assert len(blob) == blob_size
+                self._blob = blob
+                self._pos = 0
+                break
+
+    def unpack(self):
+        self.decompress()
+        blob_format = self.read_string()
+        if blob_format == 'mYm':
+            return self.read_mym_data(n_bytes=-1)
+
+    def read_mym_data(self, n_bytes=None):
+        if n_bytes is not None:
+            if n_bytes == -1:
+                n_bytes = len(self._blob) - self.pos
+            n_bytes -= 1
+
+        type_id = self.read_value('c')
+        if type_id == b'A':
+            return self.read_array(n_bytes=n_bytes)
+        elif type_id == b'S':
+            return self.read_structure(n_bytes=n_bytes)
+        elif type_id == b'C':
+            return self.read_cell_array(n_bytes=n_bytes)
+
+    def read_array(self, advance=True, n_bytes=None):
+        start = self.pos
+        n_dims = int(self.read_value('uint64'))
+        shape = self.read_value('uint64', count=n_dims)
+        n_elem = int(np.prod(shape))
+        dtype_id = self.read_value('uint32')
+        dtype = dtype_list[dtype_id]
+        is_complex = self.read_value('uint32')
+
+        if dtype_id == 4: # if dealing with character array
+            data = self.read_value(dtype, count=2 * n_elem)
+            data = data[::2].astype('<U1')
+            if n_dims == 2 and shape[0] == 1:
+                compact = data.squeeze()
+                data = compact if compact.shape == () else np.array(''.join(data.squeeze()))
+                shape = (1,)
+        else:
+            data = self.read_value(dtype, count=n_elem)
+            if is_complex:
+                data = data[:n_elem//2] + 1j * data[n_elem//2:]
+
+        if n_bytes is not None:
+            assert self.pos - start == n_bytes
+
+        if not advance:
+            self.pos = start
+
+        return self.simplify(data.reshape(shape, order='F'))
+
+    def read_structure(self, advance=True, n_bytes=None):
+        start = self.pos
+        n_dims = self.read_value('uint64').item()
+        shape = self.read_value('uint64', count=n_dims)
+        n_elem = int(np.prod(shape))
+        n_field = int(self.read_value('uint32'))
+        field_names = []
+        for i in range(n_field):
+            field_names.append(self.read_string())
+        if not field_names:
+            # return an empty array
+            return np.array(None)
+        dt = [(f, np.object) for f in field_names]
+        raw_data = []
+        for k in range(n_elem):
+            vals = []
+            for i in range(n_field):
+                nb = int(self.read_value('uint64')) # dealing with a weird bug of numpy
+                vals.append(self.read_mym_data(n_bytes=nb))
+            raw_data.append(tuple(vals))
+        data = np.rec.array(raw_data, dtype=dt)
+        if n_bytes is not None:
+            assert self.pos - start == n_bytes
+        if not advance:
+            self.pos = start
+        return self.simplify(data.reshape(shape, order='F'))
+
+    def simplify(self, array):
+        """
+        Simplify the given array as much as possible - squeeze out all singleton
+        dimensions and also convert a zero dimensional array into array scalar
+        """
+        if not self._simplify:
+            return array
+        array = array.copy()
+        array = array.squeeze()
+        if array.ndim == 0:
+            array = array[()]
+        return array
+
+    def read_cell_array(self, advance=True, n_bytes=None):
+        start = self.pos
+        n_dims = self.read_value('uint64').item()
+        shape = self.read_value('uint64', count=n_dims)
+        n_elem = int(np.prod(shape))
+        data = np.empty(n_elem, dtype=np.object)
+        for i in range(n_elem):
+            nb = self.read_value('uint64').item()
+            data[i] = self.read_mym_data(n_bytes=nb)
+        if n_bytes is not None:
+            assert self.pos - start == n_bytes
+        if not advance:
+            self.pos = start
+        return data
+
+    def read_string(self, advance=True):
+        """
+        Read a string terminated by null byte '\0'. The returned string
+        object is ASCII decoded, and will not include the terminating null byte.
+        """
+        target = self._blob.find(b'\0', self.pos)
+        assert target >= self._pos
+        data = self._blob[self._pos:target]
+        if advance:
+            self._pos = target + 1
+        return data.decode('ascii')
+
+    def read_value(self, dtype='uint64', count=1, advance=True):
+        """
+        Read one or more scalars of the indicated dtype. Count specifies the number of
+        scalars to be read in.
+        """
+        data = np.frombuffer(self._blob, dtype=dtype, count=count, offset=self.pos)
+        if advance:
+            # probably the same thing as data.nbytes * 8
+            self._pos += data.dtype.itemsize * data.size
+        if count == 1:
+            data = data[0]
+        return data
+
+    def __repr__(self):
+        return repr(self._blob[self.pos:])
+
+    def __str__(self):
+        return str(self._blob[self.pos:])
+
 
 
 def pack(obj):
@@ -48,8 +216,8 @@ def pack(obj):
     if is_complex:
         obj, imaginary = np.real(obj), np.imag(obj)
 
-    type_number = reverseClassID[obj.dtype]
-    assert dtypeList[type_number] == obj.dtype, 'ambigous or unknown array type'
+    type_number = rev_class_id[obj.dtype]
+    assert dtype_list[type_number] == obj.dtype, 'ambiguous or unknown array type'
     blob += np.asarray(type_number, dtype=np.uint32).tostring()
     blob += np.int8(is_complex).tostring() + b'\0\0\0'
     blob += obj.tostring()
@@ -64,38 +232,8 @@ def pack(obj):
 
 
 def unpack(blob):
-    """
-    Unpacks blob  data into a numpy array.
-
-    :param blob: mysql blob
-    :returns: unpacked data
-    :rtype: numpy.ndarray
-    """
-    # decompress if necessary
     if blob is None:
         return None
 
-    if blob[0:5] == b'ZL123':
-        blob_length = np.fromstring(blob[6:14], dtype=np.uint64)[0]
-        blob = zlib.decompress(blob[14:])
-        assert len(blob) == blob_length
+    return BlobReader(blob).unpack()
 
-    blob_type = blob[4]
-    if blob_type != 65:  # TODO: also process structure arrays, cell arrays, etc.
-        raise DataJointError('only arrays are currently allowed in blobs')
-    p = 5
-    dimensions = np.fromstring(blob[p:p+8], dtype=np.uint64)
-    p += 8
-    array_shape = np.fromstring(blob[p:p+8*dimensions], dtype=np.uint64)
-    p += 8 * dimensions
-    type_number = np.fromstring(blob[p:p+4], dtype=np.uint32)[0]
-    dtype = dtypeList[type_number]
-    if dtype is None:
-        raise DataJointError('Unsupported MATLAB data type '+type_number+' in blob')
-    p += 4
-    is_complex = np.fromstring(blob[p:p+4], dtype=np.uint32)[0]
-    p += 4
-    obj = np.fromstring(blob[p:], dtype=dtype)
-    if is_complex:
-        obj = obj[:len(obj)/2] + 1j*obj[len(obj)/2:]
-    return obj.reshape(array_shape)
