be834452d108be7c8956453ca51a438076d8d68d
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2016-10-06 17:53:20 -0500

    added more tests for dj.View

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,8 +1,26 @@
-from nose.tools import assert_dict_equal
+from nose.tools import assert_dict_equal, raises
+import datajoint as dj
 from . import schema
 
 
 def test_view():
     view = schema.Glot()
     assert_dict_equal(dict(view.fetch()), dict(view.definition.fetch()))
-    
+
+
+@raises(dj.DataJointError)
+def missing_definition():
+    class BadView(dj.View):
+        misspelled = schema.Glot()
+    BadView()
+
+
+@raises(dj.DataJointError)
+def missing_definition():
+    class BadView(dj.View):
+        definition = """
+        id : int
+        ---
+        value : double
+        """
+    BadView()
