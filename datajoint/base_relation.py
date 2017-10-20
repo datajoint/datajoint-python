3baf48782828aadf518e638fbfa20747eb3ef20d
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2017-10-19 21:36:52 -0500

    simplified insert from select

--- datajoint/base_relation.py
+++ datajoint/base_relation.py
@@ -137,32 +137,24 @@ class BaseRelation(RelationalOperand):
         >>>     dict(subject_id=8, species="mouse", date_of_birth="2014-09-02")])
         """
 
-        if isinstance(rows, RelationalOperand):
-            # INSERT FROM SELECT - build alternate field-narrowing query (only) when needed
-            if ignore_extra_fields and not all(name in self.heading for name in rows.heading):
-                query = 'INSERT{ignore} INTO {table} ({fields}) SELECT {fields} FROM ({select}) as `__alias`'.format(
-                ignore=" IGNORE" if ignore_errors or skip_duplicates else "",
-                table=self.full_table_name,
-                fields='`'+'`,`'.join(name for name in self.heading if name in rows.heading) + '`',
-                select=rows.make_sql())
-            else:
-                query = 'INSERT{ignore} INTO {table} ({fields}) {select}'.format(
-                ignore=" IGNORE" if ignore_errors or skip_duplicates else "",
-                table=self.full_table_name,
-                fields='`'+'`,`'.join(rows.heading.names)+'`',
-                select=rows.make_sql())
-            try:
-                self.connection.query(query)
-            except pymysql.err.InternalError as err:
-                if err.args[0] == server_error_codes['unknown column']:
-                    print(query)
-                    # args[1] -> Unknown column 'extra' in 'field list'
-                    raise DataJointError('%s : To ignore extra fields, set ignore_extra_fields=True in insert.' % err.args[1])
-                else:
-                    raise
-            return
-
         heading = self.heading
+        if isinstance(rows, RelationalOperand):
+            # insert from select
+            if not ignore_extra_fields:
+		try:
+		    raise DataJointError("Attribute %s not found.", 
+                        next(name for name in rows.heading if name not in heading))
+                except StopIteration:
+                    pass
+            fields='`'+'`,`'.join(name for name in heading if name in rows.heading) + '`'
+	    query = 'INSERT{ignore} INTO {table} ({fields}) {select}'.format( 
+	       ignore=" IGNORE" if ignore_errors or skip_duplicates else "",
+	       fields=fields,
+	       table=self.full_table_name,
+	       select=rows.make_sql(select_fields=fields)))
+            self.connection.query(query)
+            return 
+
         if heading.attributes is None:
             logger.warning('Could not access table {table}'.format(table=self.full_table_name))
             return
