3f3ead5119c4a4651cf0a94e84ae426bcaed0927
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2017-12-13 18:35:15 -0600

    remove external garbage_count

--- datajoint/external.py
+++ datajoint/external.py
@@ -70,7 +70,7 @@ class ExternalTable(BaseRelation):
                     safe_write(full_path, blob)
         else:
             raise DataJointError('Unknown external storage protocol {protocol} for {store}'.format(
-                store=store, protocol=protocol))
+                store=store, protocol=spec['protocol']))
 
         # insert tracking info
         self.connection.query(
@@ -126,16 +126,6 @@ class ExternalTable(BaseRelation):
         FROM information_schema.key_column_usage
         WHERE referenced_table_name="{tab}" and referenced_table_schema="{db}"
         """.format(tab=self.table_name, db=self.database), as_dict=True)
-
-    @property
-    def garbage_count(self):
-        """
-        :return: number of items that are no longer referenced
-        """
-        return self.connection.query(
-            "SELECT COUNT(*) FROM `{db}`.`{tab}` WHERE ".format(tab=self.table_name, db=self.database) +
-            (" AND ".join('hash NOT IN (SELECT {column_name} FROM {referencing_table})'.format(**ref)
-                          for ref in self.references) or "TRUE")).fetchone()[0]
 
     def delete(self):
         return self.delete_quick()
