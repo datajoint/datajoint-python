0c0bae4ab83914b1b7b3ee925589e1b1db2c8c74
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2017-12-21 21:52:36 -0600

    implement bucket creation and garbage collection

--- datajoint/s3.py
+++ datajoint/s3.py
@@ -2,14 +2,15 @@
 AWS S3 operations
 """
 from io import BytesIO
-from minio import Minio   # https://docs.minio.io/docs/python-client-api-reference
+import minio   # https://docs.minio.io/docs/python-client-api-reference
+import warnings
 
 class Folder:
     """
     An S3 instance manipulates a folder of objects in AWS S3
     """
     def __init__(self, endpoint, bucket, access_key, secret_key, location, database, **_):
-        self.client = Minio(endpoint, access_key=access_key, secret_key=secret_key, secure=False)
+        self.client = minio.Minio(endpoint, access_key=access_key, secret_key=secret_key, secure=False)
         self.bucket = bucket
         self.remote_path = '/'.join((location.lstrip('/'), database))
 
@@ -17,14 +18,21 @@ class Folder:
         self.client.make_bucket(self.bucket)
 
     def put(self, blob_hash, blob):
-        self.client.put_object(self.bucket, '/'.join((self.remote_path, blob_hash)), BytesIO(blob), len(blob))
+        try:
+            self.client.put_object(self.bucket, '/'.join((self.remote_path, blob_hash)), BytesIO(blob), len(blob))
+        except minio.error.NoSuchBucket:
+            warnings.warn('Creating bucket "%s"' % self.bucket)
+            self.client.make_bucket(self.bucket)
+            self.put(blob_hash, blob)
 
     def get(self, blob_hash):
-        return self.get_object(self.bucket, '/'.join((self.remote_path, blob_hash))).data
+        return self.client.get_object(self.bucket, '/'.join((self.remote_path, blob_hash))).data
 
     def clean(self, except_list):
         """
         Delete all objects except for those in the except_list
         :param except_list: a list of blob_hashes to skip.
+        :return: an iteratore of objects that failed to delete
         """
-        raise NotImplementedError
+        return self.client.remove_objects(
+            x for x in self.client.list_object(self.bucket, self.client.remote_path + '/') if x not in except_list)
