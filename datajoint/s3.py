d5e07831b062d3a60cee3043b148970a3d52be91
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2017-12-21 22:15:04 -0600

    add max_count argument to s3.Folder.clean

--- datajoint/s3.py
+++ datajoint/s3.py
@@ -4,10 +4,12 @@ AWS S3 operations
 from io import BytesIO
 import minio   # https://docs.minio.io/docs/python-client-api-reference
 import warnings
+import itertools
+
 
 class Folder:
     """
-    An S3 instance manipulates a folder of objects in AWS S3
+    A Folder instance manipulates a flat folder of objects within an S3-compatible object store
     """
     def __init__(self, endpoint, bucket, access_key, secret_key, location, database, **_):
         self.client = minio.Minio(endpoint, access_key=access_key, secret_key=secret_key, secure=False)
@@ -28,11 +30,13 @@ class Folder:
     def get(self, blob_hash):
         return self.client.get_object(self.bucket, '/'.join((self.remote_path, blob_hash))).data
 
-    def clean(self, except_list):
+    def clean(self, except_list, max_count=None):
         """
         Delete all objects except for those in the except_list
         :param except_list: a list of blob_hashes to skip.
-        :return: an iteratore of objects that failed to delete
+        :param max_count: maximum number of object to delete
+        :return: a generator of objects that failed to delete
         """
-        return self.client.remove_objects(
-            x for x in self.client.list_object(self.bucket, self.client.remote_path + '/') if x not in except_list)
+        return self.client.remove_objects(itertools.islice(
+            (x for x in self.client.list_object(self.bucket, self.client.remote_path + '/') if x not in except_list),
+            max_count))
