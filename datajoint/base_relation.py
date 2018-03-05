c5845040044c02d6e56924f8ea476dafeebe60a5
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2018-03-05 14:44:36 -0600

    fix #451 ambiguous field name in insert with skip_duplicates=True

--- datajoint/base_relation.py
+++ datajoint/base_relation.py
@@ -172,7 +172,8 @@ class BaseRelation(RelationalOperand):
                 fields='`' + '`,`'.join(fields) + '`',
                 table=self.full_table_name,
                 select=rows.make_sql(select_fields=fields),
-                duplicate=(' ON DUPLICATE KEY UPDATE `{pk}`=`{pk}`'.format(pk=self.primary_key[0])
+                duplicate=(' ON DUPLICATE KEY UPDATE `{pk}`=`{table}`.`{pk}`'.format(
+                    table=self.full_table_name, pk=self.primary_key[0])
                            if skip_duplicates else ''))
             self.connection.query(query)
             return
