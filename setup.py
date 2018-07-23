c1d3bbae225206e49316ef46a915c2d812541aad
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2018-07-23 10:47:43 -0500

    update release info for version 0.10.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 from os import path
 import sys
 
-if sys.version_info < (3,4):
+if sys.version_info < (3, 4):
     sys.exit('DataJoint is only supported on Python 3.4 or higher')
 
 here = path.abspath(path.dirname(__file__))
 
-long_description = "An object-relational mapping and relational algebra to facilitate data definition and data manipulation in MySQL databases."
+long_description = "A relational data framework for scientific data pipelines with MySQL backend."
 
 # read in version number
 with open(path.join(here, 'datajoint', 'version.py')) as f:
@@ -20,12 +20,12 @@ with open(path.join(here, 'requirements.txt')) as f:
 setup(
     name='datajoint',
     version=__version__,
-    description="An ORM with closed relational algebra",
+    description="A relational data pipeline framework.",
     long_description=long_description,
     author='Dimitri Yatsenko',
-    author_email='Dimitri.Yatsenko@gmail.com',
+    author_email='info@datajoint.io',
     license="GNU LGPL",
-    url='https://github.com/datajoint/datajoint-python',
+    url='https://datajoint.io',
     keywords='database organization',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=requirements,
