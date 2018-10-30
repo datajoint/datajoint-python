datajoint/datajoint-python

commit a3f26c2d08898211583f3a782ca1551ec0fc1460

admin.py: prompt user to update dj_local_conf.json after password update (#508)

datajoint/admin.py
@@ -1,3 +1,4 @@
+import re
 import pymysql
 from . import conn
 from getpass import getpass
@@ -14,6 +15,10 @@ def set_password(new_password=None, connection=None):   # pragma: no cover
     connection.query("SET PASSWORD = PASSWORD('%s')" % new_password)
     print('Password updated.')
 
+    if re.match('([Yy]|[Yy][Ee][Ss])', input('update dj_local_conf.json? ')):
+        dj.config['database.password'] = new_password
+        dj.config.save_local()
+        print('dj_local_conf.json updated.')
 
 def kill(restriction=None, connection=None):  # pragma: no cover
     """

commit 52af72c40eba0c8c9f27c414431b734e171202dc

fix bug introduced in a3f26c2d0

datajoint/__init__.py
@@ -46,6 +46,7 @@ except StopIteration:
     config.add_history('No config file found, using default settings.')
 else:
     config.load(config_file)
+del config_files
 
 # override login credentials with environment variables
 mapping = {k: v for k, v in zip(

datajoint/admin.py
@@ -1,7 +1,7 @@
-import re
 import pymysql
-from . import conn
+from . import conn, config
 from getpass import getpass
+from .utils import user_choice
 
 
 def set_password(new_password=None, connection=None, update_config=None):   # pragma: no cover
@@ -15,14 +15,10 @@ def set_password(new_password=None, connection=None, update_config=None):   # pr
     connection.query("SET PASSWORD = PASSWORD('%s')" % new_password)
     print('Password updated.')
 
-    if update_config is None:
-        update_config = re.match('([Yy]|[Yy][Ee][Ss])', 
-            input('update dj_local_conf.json? '))
+    if update_config or user_choice('Update local setting?') == 'yes':
+        config['database.password'] = new_password
+        config.save_local(verbose=True)
 
-    if update_config:
-        dj.config['database.password'] = new_password
-        dj.config.save_local()
-        print('dj_local_conf.json updated.')
 
 def kill(restriction=None, connection=None):  # pragma: no cover
     """

datajoint/settings.py
@@ -90,13 +90,15 @@ class Config(collections.MutableMapping):
     def __len__(self):
         return len(self.instance._conf)
 
-    def save(self, filename):
+    def save(self, filename, verbose=False):
         """
         Saves the settings in JSON format to the given file path.
         :param filename: filename of the local JSON settings file.
         """
         with open(filename, 'w') as fid:
             json.dump(self._conf, fid, indent=4)
+        if verbose:
+            print('Saved settings in ' + filename)
 
     def load(self, filename):
         """
@@ -109,17 +111,17 @@ class Config(collections.MutableMapping):
             self._conf.update(json.load(fid))
         self.add_history('Updated from config file: %s' % filename)
 
-    def save_local(self):
+    def save_local(self, verbose=False):
         """
         saves the settings in the local config file
         """
-        self.save(LOCALCONFIG)
+        self.save(LOCALCONFIG, verbose)
 
-    def save_global(self):
+    def save_global(self, verbose=False):
         """
         saves the settings in the global config file
         """
-        self.save(os.path.expanduser(os.path.join('~', GLOBALCONFIG)))
+        self.save(os.path.expanduser(os.path.join('~', GLOBALCONFIG)), verbose)
 
     @contextmanager
     def __call__(self, **kwargs):

datajoint/utils.py
@@ -4,6 +4,7 @@ import re
 import os
 from .errors import DataJointError
 
+
 class ClassProperty:
     def __init__(self, f):
         self.f = f
@@ -21,7 +22,7 @@ def user_choice(prompt, choices=("yes", "no"), default=None):
     :param default: default choice
     :return: the user's choice
     """
-    assert default is None or  default in choices
+    assert default is None or default in choices
     choice_list = ', '.join((choice.title() if choice == default else choice for choice in choices))
     response = None
     while response not in choices:
@@ -38,9 +39,7 @@ def to_camel_case(s):
     :returns: string in CamelCase notation
 
     Example:
-
     >>> to_camel_case("table_name") # yields "TableName"
-
     """
 
     def to_upper(match):