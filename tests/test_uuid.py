469d2591d379b1457fcae35f343a686c2ace0fa6
Dimitri Yatsenko <dimitri@vathes.com>
2019-02-21 17:01:13 -0600

    improve uuid test

--- tests/test_uuid.py
+++ tests/test_uuid.py
@@ -8,7 +8,9 @@ def test_uuid():
     u, n = uuid.uuid4(), -1
     Item().insert1(dict(item=u, number=n))
     Item().insert(zip(map(uuid.uuid1, range(20)), count()))
-    keys = Item().fetch('KEY')
-    keys_array = Item().fetch('KEY_ARRAY')
+    keys, key_array = Item().fetch('KEY', 'KEY_ARRAY')
+    assert_equal(keys[0]['item'], key_array['item'][0])
     number = (Item() & {'item': u}).fetch1('number')
     assert_equal(number, n)
+    item = (Item & {'number': n}).fetch1('item')
+    assert_equal(u, item)
