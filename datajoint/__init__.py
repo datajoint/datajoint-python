d604d0b6b1097210680dadcc6ab5f10932067bea
Dimitri Yatsenko <dimitri@vathes.com>
2019-04-15 10:25:00 -0500

    rename `get_schema_names` to `list_schemas`

--- datajoint/__init__.py
+++ datajoint/__init__.py
@@ -18,7 +18,7 @@ __author__ = "DataJoint Contributors"
 __date__ = "February 7, 2019"
 __all__ = ['__author__', '__version__',
            'config', 'conn', 'Connection',
-           'schema', 'create_virtual_module', 'get_schema_names',
+           'schema', 'create_virtual_module', 'list_schemas',
            'Table', 'FreeTable',
            'Manual', 'Lookup', 'Imported', 'Computed', 'Part',
            'Not', 'AndList', 'U', 'Diagram', 'Di', 'ERD',
@@ -31,7 +31,7 @@ from .version import __version__
 from .settings import config
 from .connection import conn, Connection
 from .schema import Schema as schema
-from .schema import create_virtual_module, get_schema_names
+from .schema import create_virtual_module, list_schemas
 from .table import Table, FreeTable
 from .user_tables import Manual, Lookup, Imported, Computed, Part
 from .expression import Not, AndList, U
