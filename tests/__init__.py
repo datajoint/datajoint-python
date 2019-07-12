197a99db24f9eb618b279a25ff216ccda1064372
guzman-raphael <raphael.h.guzman@gmail.com>
2019-07-12 11:09:13 -0500

    Add comments.

--- tests/__init__.py
+++ tests/__init__.py
@@ -107,11 +107,14 @@ def setup_package():
     """
     dj.config['safemode'] = False
 
+    #Remove old S3
     objs = list(minioClient.list_objects_v2("datajoint-test-old",recursive=True))
     objs = [minioClient.remove_object("datajoint-test-old",
             o.object_name.encode('utf-8')) for o in objs]
     minioClient.remove_bucket("datajoint-test-old")
 
+    #Add old S3
+    minioClient.make_bucket("datajoint-test", location="us-east-1")
     minioClient.make_bucket("datajoint-test-old", location="us-east-1")
     for filename in os.listdir('./external-legacy-data'):
         minioClient.fput_object('datajoint-test-old', 'dj/store/djtest_extern/{}'.format(filename),
