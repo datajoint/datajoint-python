7c993016f55b53dca74455ec2b668c7699e749c5
guzman-raphael <raphael.h.guzman@gmail.com>
2020-02-03 08:46:24 -0600

    Move import of AttributeAdapter sooner to allow plugins to load it for their respective imports.

--- datajoint/__init__.py
+++ datajoint/__init__.py
@@ -29,6 +29,7 @@ __all__ = ['__author__', '__version__',
 from .version import __version__
 from .settings import config
 from .connection import conn, Connection
+from .attribute_adapter import AttributeAdapter
 from .schema import Schema as schema
 from .schema import create_virtual_module, list_schemas
 from .table import Table, FreeTable
@@ -38,7 +39,6 @@ from .diagram import Diagram
 from .admin import set_password, kill
 from .blob import MatCell, MatStruct
 from .fetch import key
-from .attribute_adapter import AttributeAdapter
 from . import errors
 from .errors import DataJointError
 from .migrate import migrate_dj011_external_blob_storage_to_dj012
