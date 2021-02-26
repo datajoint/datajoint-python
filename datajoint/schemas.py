cd30e39fcbeae66701f11dc9aebf097478f559f4
Dimitri Yatsenko <dimitri@vathes.com>
2021-02-26 17:15:56 -0600

    reformat SQL query
    Co-authored-by: Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>

--- datajoint/schemas.py
+++ datajoint/schemas.py
@@ -299,7 +299,10 @@ class Schema:
         if self.database is None:
             raise DataJointError("Schema must be activated first.")
         return self.database is not None and (
-            self.connection.query("SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE '{database}'".format(
+            self.connection.query(
+                "SELECT schema_name "
+                "FROM information_schema.schemata "
+                "WHERE schema_name = '{database}'".format(
                 database=self.database)).rowcount > 0)
 
     @property
