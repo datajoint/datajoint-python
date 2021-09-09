08a3fd8a0c9a53acbf9135622b153aaf7385bfb7
jverswijver <jeroen.verswijver@gmail.com>
2021-09-09 13:26:29 -0500

    Change MinioAdmin to terminal commands, PEP8 fmt.

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,12 +1,13 @@
 from . import S3_CONN_INFO
 from minio import Minio
-import minio
 import json
 import urllib3
 import certifi
 from nose.tools import assert_true, raises
 from .schema_external import schema, SimpleRemote
 from datajoint.errors import DataJointError
+import os
+
 
 class TestS3:
 
@@ -31,7 +32,7 @@ class TestS3:
             http_client=http_client)
 
         assert_true(minio_client.bucket_exists(S3_CONN_INFO['bucket']))
-        
+
     @staticmethod
     def test_connection_secure():
 
@@ -53,66 +54,64 @@ class TestS3:
             http_client=http_client)
 
         assert_true(minio_client.bucket_exists(S3_CONN_INFO['bucket']))
+
     @staticmethod
     @raises(DataJointError)
     def test_remove_object_exception():
         # https://github.com/datajoint/datajoint-python/issues/952
-        
+
         # Initialize minioClient with an endpoint and access/secret keys.
         minio_client = Minio(
             'minio:9000',
-            access_key= 'jeffjeff',
-            secret_key= 'jeffjeff',
+            access_key='jeffjeff',
+            secret_key='jeffjeff',
             secure=False)
 
-        minio_admin = minio.MinioAdmin(target = 'myminio')
-        
         # Create new user
-        minio_admin.user_add(access_key = 'jeffjeff', secret_key = 'jeffjeff')
-
+        os.system('mc admin user add myminio jeffjeff jeffjeff')
         # json for test policy for permissionless user
         testpolicy = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "s3:GetBucketLocation",
-                "s3:ListBucket",
-                "s3:ListBucketMultipartUploads",
-                "s3:ListAllMyBuckets"
-            ],
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:s3:::datajoint.test",
-                "arn:aws:s3:::datajoint.migrate"
-            ],
-            "Sid": ""
-        },
-        {
-            "Action": [
-                "s3:GetObject",
-                "s3:ListMultipartUploadParts"
-            ],
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:s3:::datajoint.test/*",
-                "arn:aws:s3:::datajoint.migrate/*"
-            ],
-            "Sid": ""
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": [
+                        "s3:GetBucketLocation",
+                        "s3:ListBucket",
+                        "s3:ListBucketMultipartUploads",
+                        "s3:ListAllMyBuckets"
+                    ],
+                    "Effect": "Allow",
+                    "Resource": [
+                        "arn:aws:s3:::datajoint.test",
+                        "arn:aws:s3:::datajoint.migrate"
+                    ],
+                    "Sid": ""
+                },
+                {
+                    "Action": [
+                        "s3:GetObject",
+                        "s3:ListMultipartUploadParts"
+                    ],
+                    "Effect": "Allow",
+                    "Resource": [
+                        "arn:aws:s3:::datajoint.test/*",
+                        "arn:aws:s3:::datajoint.migrate/*"
+                    ],
+                    "Sid": ""
+                }
+            ]
         }
-    ]
-}
 
         # Write test json to tmp directory so we can use it to create a new user policy
         with open('/tmp/policy.json', 'w') as f:
             f.write(json.dumps(testpolicy))
-        
+
         # Add the policy and apply it to the user
-        minio_admin.policy_add(policy_name = 'test', policy_file = '/tmp/policy.json')
-        minio_admin.policy_set(policy_name = 'test', user = 'jeffjeff')
+        os.system('mc admin policy add myminio test /tmp/policy.json')
+        os.system('mc admin policy set myminio test user=jeffjeff')
 
         # Insert some test data and remove it so that the external table is populated
-        test = [1,[1,2,3]]
+        test = [1, [1, 2, 3]]
         SimpleRemote.insert1(test)
         SimpleRemote.delete()
 
@@ -121,13 +120,16 @@ class TestS3:
 
         # Apply our new minio client to the external table that has permissions restrictions
         schema.external['share'].s3.client = minio_client
-        
+
         # This method returns a list of errors
-        error_list = schema.external['share'].delete(delete_external_files = True, errors_as_string = False)
+        error_list = schema.external['share'].delete(delete_external_files=True, errors_as_string=False)
 
         # Teardown
-        minio_admin.user_remove(access_key = 'jeffjeff')
-        minio_admin.policy_remove('test')
+        os.system('mc admin policy remove myminio test')
+        os.system('mc admin user remove myminio jeffjeff')
+
+        # minio_admin.user_remove(access_key = 'jeffjeff')
+        # minio_admin.policy_remove('test')
         schema.external['share'].s3.client = old_client
 
         # Raise the error we want
