22eb1c38d97ee18251333da28a3fbbfdeecd7e45
jverswijver <49455164+jverswijver@users.noreply.github.com>
2021-10-07 14:35:33 -0500

    Update tests/test_groupby.py comment
    Co-authored-by: Dimitri Yatsenko <dimitri@datajoint.com>

--- tests/test_groupby.py
+++ tests/test_groupby.py
@@ -3,8 +3,9 @@ from .schema_simple import A, D
 
 def test_aggr_with_proj():
     # issue #944 - only breaks with MariaDB
-    # Maria db wants all the fields in the select statement to be in the group by 
-    # even if the fields are in fuctional dependency, this is incorrect and MySQL 
-    # implements this properly
-    # More info on the issue page
+    # MariaDB implements the SQL:1992 standard that prohibits fields in the select statement that are 
+    # not also in the GROUP BY statement.
+    # An improved specification in SQL:2003 allows fields that are functionally dependent on the group by 
+    # attributes to be allowed in the select. This behavior is implemented by MySQL correctly but not MariaDB yet. 
+    # See MariaDB issue: https://jira.mariadb.org/browse/MDEV-11588
     A.aggr(D.proj(m='id_l'), ..., n='max(m) - min(m)')
