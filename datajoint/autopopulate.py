2d628355c1cf1effb5133db2a816ea7d4c06fed7
Hendrik Heiser <40271452+Hendriela@users.noreply.github.com>
2021-11-01 13:44:27 +0100

    Custom arguments are passed to populate() in an explicit "make_kwargs" argument.

--- datajoint/autopopulate.py
+++ datajoint/autopopulate.py
@@ -96,7 +96,7 @@ class AutoPopulate:
 
     def populate(self, *restrictions, suppress_errors=False, return_exception_objects=False,
                  reserve_jobs=False, order="original", limit=None, max_calls=None,
-                 display_progress=False, **kwargs):
+                 display_progress=False, make_kwargs=None):
         """
         rel.populate() calls rel.make(key) for every primary key in self.key_source
         for which there is not already a tuple in rel.
@@ -108,7 +108,7 @@ class AutoPopulate:
         :param display_progress: if True, report progress_bar
         :param limit: if not None, checks at most that many keys
         :param max_calls: if not None, populates at max that many keys
-        :param kwargs: additional custom keyword arguments that will be passed down to each make() call
+        :param kwargs: optional dict containing arguments that will be passed down to each make() call
         """
         if self.connection.in_transaction:
             raise DataJointError('Populate cannot be called during a transaction.')
@@ -151,8 +151,8 @@ class AutoPopulate:
                     call_count += 1
                     self.__class__._allow_insert = True
                     try:
-                        if kwargs:
-                            make(dict(key), **kwargs)
+                        if make_kwargs is not None:
+                            make(dict(key), **make_kwargs)
                         else:
                             make(dict(key))
                     except (KeyboardInterrupt, SystemExit, Exception) as error:
