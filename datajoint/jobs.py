e1b4524422f84a428d92553a1ae281cf0d40abff
Chris Roat <1053153+chrisroat@users.noreply.github.com>
2022-01-20 11:31:14 -0800

    Changes ~jobs.error_stack from blob to mediumblob

--- datajoint/jobs.py
+++ datajoint/jobs.py
@@ -32,7 +32,7 @@ class JobTable(Table):
         status  :enum('reserved','error','ignore')  # if tuple is missing, the job is available
         key=null  :blob  # structure containing the key
         error_message=""  :varchar({error_message_length})  # error message returned if failed
-        error_stack=null  :blob  # error stack if failed
+        error_stack=null  :mediumblob  # error stack if failed
         user="" :varchar(255) # database user
         host=""  :varchar(255)  # system hostname
         pid=0  :int unsigned  # system process id
