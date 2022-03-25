98201fa81590f45189bfdc52757f4adf3d0296e9
A-Baji <bidabaji@gmail.com>
2022-03-25 17:28:52 -0500

    format missed files

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -133,30 +133,33 @@ def test_overlapping_name():
 
 def test_list_tables():
     # https://github.com/datajoint/datajoint-python/issues/838
-    assert set(
-        [
-            "reserved_word",
-            "#l",
-            "#a",
-            "__d",
-            "__b",
-            "__b__c",
-            "__e",
-            "__e__f",
-            "#outfit_launch",
-            "#outfit_launch__outfit_piece",
-            "#i_j",
-            "#j_i",
-            "#t_test_update",
-            "#data_a",
-            "#data_b",
-            "f",
-            "#argmax_test",
-            "#website",
-            "profile",
-            "profile__website",
-        ]
-    ) == set(schema_simple.list_tables())
+    assert (
+        set(
+            [
+                "reserved_word",
+                "#l",
+                "#a",
+                "__d",
+                "__b",
+                "__b__c",
+                "__e",
+                "__e__f",
+                "#outfit_launch",
+                "#outfit_launch__outfit_piece",
+                "#i_j",
+                "#j_i",
+                "#t_test_update",
+                "#data_a",
+                "#data_b",
+                "f",
+                "#argmax_test",
+                "#website",
+                "profile",
+                "profile__website",
+            ]
+        )
+        == set(schema_simple.list_tables())
+    )
 
 
 def test_schema_save():
