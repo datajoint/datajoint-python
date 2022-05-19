6b90a304cde059d00d61a54d43ee8090d0293f73
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2022-05-19 15:02:59 -0500

    drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from os import path
 import sys
 
-min_py_version = (3, 6)
+min_py_version = (3, 7)
 
 if sys.version_info <  min_py_version:
     sys.exit('DataJoint is only supported for Python {}.{} or higher'.format(*min_py_version))
