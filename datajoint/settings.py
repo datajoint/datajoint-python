6a0047bd44218b32dfe8c85a99aee50014c4ba33
Joseph Burling <joseph@datajoint.com>
2022-08-19 12:31:39 -0500

    fix(logging): :bug: fix arguments sent to logger during config update
    --- Logging error --- `TypeError: not all arguments converted during string formatting`

--- datajoint/settings.py
+++ datajoint/settings.py
@@ -241,9 +241,7 @@ class Config(collections.abc.MutableMapping):
             return self._conf[key]
 
         def __setitem__(self, key, value):
-            logger.debug(
-                logging.DEBUG, "Setting {0:s} to {1:s}".format(str(key), str(value))
-            )
+            logger.debug("Setting {0:s} to {1:s}".format(str(key), str(value)))
             if validators[key](value):
                 self._conf[key] = value
             else:
