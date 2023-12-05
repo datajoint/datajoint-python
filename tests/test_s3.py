f993076e8c9f1a2f7ac8f2b4cedf297cbccf1938
Ethan Ho <ethan.n.ho@gmail.com>
2023-12-05 12:17:56 -0600

    Change scope of raises block

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -58,36 +58,35 @@ class TestS3:
     def test_connection_secure(self, minio_client):
         assert minio_client.bucket_exists(S3_CONN_INFO["bucket"])
 
-    def test_remove_object_exception(self):
-        # TODO: mv to failing block
+    def test_remove_object_exception(self, schema_ext):
+        # https://github.com/datajoint/datajoint-python/issues/952
+
+        # Insert some test data and remove it so that the external table is populated
+        test = [1, [1, 2, 3]]
+        SimpleRemote.insert1(test)
+        SimpleRemote.delete()
+
+        # Save the old external table minio client
+        old_client = schema_ext.external["share"].s3.client
+
+        # Apply our new minio client which has a user that does not exist
+        schema_ext.external["share"].s3.client = Minio(
+            S3_CONN_INFO["endpoint"],
+            access_key="jeffjeff",
+            secret_key="jeffjeff",
+            secure=False,
+        )
+
+        # This method returns a list of errors
+        error_list = schema_ext.external["share"].delete(
+            delete_external_files=True, errors_as_string=False
+        )
+
+        # Teardown
+        schema_ext.external["share"].s3.client = old_client
+        schema_ext.external["share"].delete(delete_external_files=True)
+
         with pytest.raises(DataJointError):
-            # https://github.com/datajoint/datajoint-python/issues/952
-
-            # Insert some test data and remove it so that the external table is populated
-            test = [1, [1, 2, 3]]
-            SimpleRemote.insert1(test)
-            SimpleRemote.delete()
-
-            # Save the old external table minio client
-            old_client = schema.external["share"].s3.client
-
-            # Apply our new minio client which has a user that does not exist
-            schema.external["share"].s3.client = Minio(
-                S3_CONN_INFO["endpoint"],
-                access_key="jeffjeff",
-                secret_key="jeffjeff",
-                secure=False,
-            )
-
-            # This method returns a list of errors
-            error_list = schema.external["share"].delete(
-                delete_external_files=True, errors_as_string=False
-            )
-
-            # Teardown
-            schema.external["share"].s3.client = old_client
-            schema.external["share"].delete(delete_external_files=True)
-
             # Raise the error we want if the error matches the expected uuid
             if str(error_list[0][0]) == str(uuid_from_buffer(pack(test[1]))):
                 raise error_list[0][2]
