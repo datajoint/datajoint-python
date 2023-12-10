e97f163a6cf0984f59c27127d454dc453b0318ca
Ethan Ho <ethan.n.ho@gmail.com>
2023-12-09 21:32:58 -0700

    Clean up and format

--- tests/test_declare.py
+++ tests/test_declare.py
@@ -5,21 +5,7 @@ import inspect
 from datajoint.declare import declare
 
 
-@pytest.fixture
-def schema_any(schema_any):
-    auto = Auto()
-    auto.fill()
-    user = User()
-    subject = Subject()
-    experiment = Experiment()
-    trial = Trial()
-    ephys = Ephys()
-    channel = Ephys.Channel()
-    yield schema_any
-
-
 class TestDeclare:
-
     def test_schema_decorator(self, schema_any):
         assert issubclass(Subject, dj.Lookup)
         assert not issubclass(Subject, dj.Part)
@@ -60,11 +46,11 @@ class TestDeclare:
         s2 = declare(rel.full_table_name, rel.describe(), context)
         assert s1 == s2
 
-    def test_part(self):
+    def test_part(self, schema_any):
         """
         Lookup and part with the same name.  See issue #365
         """
-        local_schema = dj.Schema(schema.database)
+        local_schema = dj.Schema(schema_any.database)
 
         @local_schema
         class Type(dj.Lookup):
@@ -86,118 +72,144 @@ class TestDeclare:
                 """
 
     def test_attributes(self, schema_any):
-        # test autoincrement declaration
+        """
+        Test autoincrement declaration
+        """
+        auto = Auto()
+        auto.fill()
+        user = User()
+        subject = Subject()
+        experiment = Experiment()
+        trial = Trial()
+        ephys = Ephys()
+        channel = Ephys.Channel()
+
         assert auto.heading.names == ["id", "name"]
         assert auto.heading.attributes["id"].autoincrement
 
         # test attribute declarations
-        assert (
-            subject.heading.names ==
-            ["subject_id", "real_id", "species", "date_of_birth", "subject_notes"])
+        assert subject.heading.names == [
+            "subject_id",
+            "real_id",
+            "species",
+            "date_of_birth",
+            "subject_notes",
+        ]
         assert subject.primary_key == ["subject_id"]
         assert subject.heading.attributes["subject_id"].numeric
         assert not subject.heading.attributes["real_id"].numeric
 
-        assert (
-            experiment.heading.names ==
-            [
-                "subject_id",
-                "experiment_id",
-                "experiment_date",
-                "username",
-                "data_path",
-                "notes",
-                "entry_time",
-            ])
+        assert experiment.heading.names == [
+            "subject_id",
+            "experiment_id",
+            "experiment_date",
+            "username",
+            "data_path",
+            "notes",
+            "entry_time",
+        ]
         assert experiment.primary_key == ["subject_id", "experiment_id"]
 
-        assert (
-            trial.heading.names ==  # tests issue #516
-            ["animal", "experiment_id", "trial_id", "start_time"])
+        assert trial.heading.names == [  # tests issue #516
+            "animal",
+            "experiment_id",
+            "trial_id",
+            "start_time",
+        ]
         assert trial.primary_key == ["animal", "experiment_id", "trial_id"]
 
-        assert (
-            ephys.heading.names ==
-            ["animal", "experiment_id", "trial_id", "sampling_frequency", "duration"])
+        assert ephys.heading.names == [
+            "animal",
+            "experiment_id",
+            "trial_id",
+            "sampling_frequency",
+            "duration",
+        ]
         assert ephys.primary_key == ["animal", "experiment_id", "trial_id"]
 
-        assert (
-            channel.heading.names ==
-            ["animal", "experiment_id", "trial_id", "channel", "voltage", "current"])
-        assert (
-            channel.primary_key == ["animal", "experiment_id", "trial_id", "channel"])
+        assert channel.heading.names == [
+            "animal",
+            "experiment_id",
+            "trial_id",
+            "channel",
+            "voltage",
+            "current",
+        ]
+        assert channel.primary_key == ["animal", "experiment_id", "trial_id", "channel"]
         assert channel.heading.attributes["voltage"].is_blob
 
     def test_dependencies(self, schema_any):
+        auto = Auto()
+        auto.fill()
+        user = User()
+        subject = Subject()
+        experiment = Experiment()
+        trial = Trial()
+        ephys = Ephys()
+        channel = Ephys.Channel()
+
         assert experiment.full_table_name in user.children(primary=False)
         assert set(experiment.parents(primary=False)) == {user.full_table_name}
         assert experiment.full_table_name in user.children(primary=False)
         assert set(experiment.parents(primary=False)) == {user.full_table_name}
-        assert (
-            set(
-                s.full_table_name
-                for s in experiment.parents(primary=False, as_objects=True)
-            ) ==
-            {user.full_table_name})
+        assert set(
+            s.full_table_name
+            for s in experiment.parents(primary=False, as_objects=True)
+        ) == {user.full_table_name}
 
         assert experiment.full_table_name in subject.descendants()
-        assert (experiment.full_table_name
-            in {s.full_table_name for s in subject.descendants(as_objects=True)})
+        assert experiment.full_table_name in {
+            s.full_table_name for s in subject.descendants(as_objects=True)
+        }
         assert subject.full_table_name in experiment.ancestors()
-        assert (subject.full_table_name
-            in {s.full_table_name for s in experiment.ancestors(as_objects=True)})
+        assert subject.full_table_name in {
+            s.full_table_name for s in experiment.ancestors(as_objects=True)
+        }
 
         assert trial.full_table_name in experiment.descendants()
-        assert (trial.full_table_name
-            in {s.full_table_name for s in experiment.descendants(as_objects=True)})
+        assert trial.full_table_name in {
+            s.full_table_name for s in experiment.descendants(as_objects=True)
+        }
         assert experiment.full_table_name in trial.ancestors()
-        assert (experiment.full_table_name
-            in {s.full_table_name for s in trial.ancestors(as_objects=True)})
+        assert experiment.full_table_name in {
+            s.full_table_name for s in trial.ancestors(as_objects=True)
+        }
 
-        assert (
-            set(trial.children(primary=True)) ==
-            {ephys.full_table_name, trial.Condition.full_table_name})
+        assert set(trial.children(primary=True)) == {
+            ephys.full_table_name,
+            trial.Condition.full_table_name,
+        }
         assert set(trial.parts()) == {trial.Condition.full_table_name}
-        assert (
-            set(s.full_table_name for s in trial.parts(as_objects=True)) ==
-            {trial.Condition.full_table_name})
+        assert set(s.full_table_name for s in trial.parts(as_objects=True)) == {
+            trial.Condition.full_table_name
+        }
         assert set(ephys.parents(primary=True)) == {trial.full_table_name}
-        assert (
-            set(
-                s.full_table_name for s in ephys.parents(primary=True, as_objects=True)
-            ) ==
-            {trial.full_table_name})
+        assert set(
+            s.full_table_name for s in ephys.parents(primary=True, as_objects=True)
+        ) == {trial.full_table_name}
         assert set(ephys.children(primary=True)) == {channel.full_table_name}
-        assert (
-            set(
-                s.full_table_name for s in ephys.children(primary=True, as_objects=True)
-            ) ==
-            {channel.full_table_name})
+        assert set(
+            s.full_table_name for s in ephys.children(primary=True, as_objects=True)
+        ) == {channel.full_table_name}
         assert set(channel.parents(primary=True)) == {ephys.full_table_name}
-        assert (
-            set(
-                s.full_table_name
-                for s in channel.parents(primary=True, as_objects=True)
-            ) ==
-            {ephys.full_table_name})
+        assert set(
+            s.full_table_name for s in channel.parents(primary=True, as_objects=True)
+        ) == {ephys.full_table_name}
 
     def test_descendants_only_contain_part_table(self, schema_any):
         """issue #927"""
 
-        @schema_any
         class A(dj.Manual):
             definition = """
             a: int
             """
 
-        @schema_any
         class B(dj.Manual):
             definition = """
             -> A
             b: int
             """
 
-        @schema_any
         class Master(dj.Manual):
             definition = """
             table_master: int
@@ -209,6 +221,10 @@ class TestDeclare:
                 -> B
                 """
 
+        context = dict(A=A, B=B, Master=Master)
+        schema_any(A, context=context)
+        schema_any(B, context=context)
+        schema_any(Master, context=context)
         assert A.descendants() == [
             "`djtest_test1`.`a`",
             "`djtest_test1`.`b`",
@@ -216,7 +232,6 @@ class TestDeclare:
         ]
 
     def test_bad_attribute_name(self, schema_any):
-
         class BadName(dj.Manual):
             definition = """
             Bad_name : int
@@ -243,7 +258,6 @@ class TestDeclare:
             schema_any(B)
 
     def test_primary_nullable_foreign_key(self, schema_any):
-
         class Q(dj.Manual):
             definition = """
             -> [nullable] Experiment
@@ -253,7 +267,6 @@ class TestDeclare:
             schema_any(Q)
 
     def test_invalid_foreign_key_option(self, schema_any):
-
         class R(dj.Manual):
             definition = """
             -> Experiment
@@ -265,7 +278,6 @@ class TestDeclare:
             schema_any(R)
 
     def test_unsupported_datatype(self, schema_any):
-
         class Q(dj.Manual):
             definition = """
             experiment : int
@@ -277,7 +289,6 @@ class TestDeclare:
             schema_any(Q)
 
     def test_int_datatype(self, schema_any):
-
         @schema_any
         class Owner(dj.Manual):
             definition = """
@@ -287,7 +298,6 @@ class TestDeclare:
             """
 
     def test_unsupported_int_datatype(self, schema_any):
-
         class Driver(dj.Manual):
             definition = """
             driverid : tinyint
