f3a5dd1f36d761a0bef720c402c03c2ff27e3c85
Ethan Ho <ethan.n.ho@gmail.com>
2023-12-13 11:06:49 -0700

    nose2pytest test_reconnection

--- tests/test_reconnection.py
+++ tests/test_reconnection.py
@@ -17,19 +17,19 @@ class TestReconnect:
         self.conn = dj.conn(reset=True, **CONN_INFO)
 
     def test_close(self):
-        assert_true(self.conn.is_connected, "Connection should be alive")
+        assert self.conn.is_connected, "Connection should be alive"
         self.conn.close()
-        assert_false(self.conn.is_connected, "Connection should now be closed")
+        assert not self.conn.is_connected, "Connection should now be closed"
 
     def test_reconnect(self):
-        assert_true(self.conn.is_connected, "Connection should be alive")
+        assert self.conn.is_connected, "Connection should be alive"
         self.conn.close()
         self.conn.query("SHOW DATABASES;", reconnect=True).fetchall()
-        assert_true(self.conn.is_connected, "Connection should be alive")
+        assert self.conn.is_connected, "Connection should be alive"
 
     @raises(DataJointError)
     def test_reconnect_throws_error_in_transaction(self):
-        assert_true(self.conn.is_connected, "Connection should be alive")
+        assert self.conn.is_connected, "Connection should be alive"
         with self.conn.transaction:
             self.conn.close()
             self.conn.query("SHOW DATABASES;", reconnect=True).fetchall()
