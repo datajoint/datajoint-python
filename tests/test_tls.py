1e3ba5c3a239bcb93c0fe940a08ba807cd4a793a
Ethan Ho <ethan.n.ho@gmail.com>
2023-12-14 15:36:40 -0700

    Format with black

--- tests/test_tls.py
+++ tests/test_tls.py
@@ -24,5 +24,9 @@ def test_insecure_connection(db_creds_test, connection_test):
 def test_reject_insecure(db_creds_test, connection_test):
     with pytest.raises(OperationalError):
         dj.conn(
-            db_creds_test["host"], user="djssl", password="djssl", use_tls=False, reset=True
+            db_creds_test["host"],
+            user="djssl",
+            password="djssl",
+            use_tls=False,
+            reset=True,
         ).query("SHOW STATUS LIKE 'Ssl_cipher';").fetchone()[1]
