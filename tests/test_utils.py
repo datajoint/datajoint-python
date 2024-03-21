a9470b5516d5cef6da46deeaaf6ff6511ee2c6a3
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-03-21 21:16:24 +0000

    Enforce strict CamelCase for Table subclasses
    Per @dimitri-yatsenko suggestion.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,8 +3,21 @@ Collection of test cases to test core module.
 """
 
 from datajoint import DataJointError
-from datajoint.utils import from_camel_case, to_camel_case
+from datajoint.utils import from_camel_case, to_camel_case, is_camel_case, contains_non_ascii_char
 import pytest
+
+
+def test_is_camel_case():
+    assert is_camel_case("AllGroups")
+    assert not is_camel_case("allGroups")
+    assert not is_camel_case("repNames")
+    assert not is_camel_case("10_all")
+    assert not is_camel_case("hello world")
+    assert not is_camel_case("#baisc_names")
+    assert not is_camel_case("alphaBeta")
+    non_ascii_class_name = "TestΣ"
+    assert contains_non_ascii_char(non_ascii_class_name)
+    assert not is_camel_case(non_ascii_class_name)
 
 
 def test_from_camel_case():
