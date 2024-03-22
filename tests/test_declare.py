8755db7ed4cc8b99d4d9ee8ccc18239c8983ba68
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-03-22 13:30:20 +0000

    Fix failing test_declare test

--- tests/test_declare.py
+++ tests/test_declare.py
@@ -356,5 +356,7 @@ def test_table_name_with_underscores(schema_any):
         """
 
     schema_any(TableNoUnderscores)
-    with pytest.raises(dj.DataJointError, match="strict CamelCase") as e:
+    with pytest.raises(
+        dj.DataJointError, match="must be alphanumeric in CamelCase"
+    ) as e:
         schema_any(Table_With_Underscores)
