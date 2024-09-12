fe9f1497b42a76790891fe426b74e28027e1c487
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-09-12 15:12:24 -0500

    Loosen CLI dj.config command checks
    Config values likely changing while running concurrent tests.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -62,7 +62,7 @@ def test_cli_config():
 
     stdout, stderr = process.communicate()
     cleaned = stdout.strip(" >\t\n\r")
-    assert dj.config == ast.literal_eval(cleaned)
+    assert dj.config.keys() == ast.literal_eval(cleaned).keys()
 
 
 def test_cli_args():
