375e083a59c7f70a51f4ed064ac2e91558ec867c
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-09-19 13:29:20 -0500

    Revert "Parse DJ_VERSION from pyproject.toml"
    This reverts commit 9dbc10e3165f4b62ea56d90a0b4c3252043c2d9f.

--- datajoint/version.py
+++ datajoint/version.py
@@ -1,10 +1,10 @@
 try:
-    # Use datajoint/_version.py written by setuptools_scm if it exists
-    # This module is not tracked in VCS and defines a __version_tuple__ like
-    # (0, 14, 3, 'dev224', 'g0812fe17.d20240919')
+	# Use datajoint/_version.py written by setuptools_scm if it exists
+	# This module is not tracked in VCS and defines a __version_tuple__ like
+	# (0, 14, 3, 'dev224', 'g0812fe17.d20240919')
     from ._version import __version_tuple__ as version_tuple
 except ImportError:
-    version_tuple = (0, 14, 3)
+	version_tuple = (0, 14, 3)
 
 __version__ = ".".join(str(x) for x in version_tuple[:3])
 
